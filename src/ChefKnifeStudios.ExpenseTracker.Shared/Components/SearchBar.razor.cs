ff273013ffb4ce751be21cd68c82c62ffdf67154
Henry Faulkner <hfaulkner27@gmail.com>
2025-06-08 21:19:12 -0400

    SearchBar component

---
+++ src/ChefKnifeStudios.ExpenseTracker.Shared/Components/SearchBar.razor.cs
@@ -0,0 +1,18 @@
+﻿using ChefKnifeStudios.ExpenseTracker.Shared.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace ChefKnifeStudios.ExpenseTracker.Shared.Components;
+
+public partial class SearchBar : ComponentBase
+{
+    [Inject] ISearchViewModel SearchViewModel { get; set; } = null!;
+
+    string? _searchText = string.Empty;
+
+    async Task HandleSearch()
+    {
+        if (string.IsNullOrWhiteSpace(_searchText)) _searchText = string.Empty;
+        _searchText = _searchText.Trim();
+        await SearchViewModel.ChangeSearchTextAsync(_searchText);
+    }
+}
