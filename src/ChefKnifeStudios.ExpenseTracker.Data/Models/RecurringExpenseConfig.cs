149c8a719a7dd14adc57c41c65c5029fa057e127
Henry Faulkner <hfaulkner27@gmail.com>
2025-06-29 14:34:43 -0400

    try this

--- src/ChefKnifeStudios.ExpenseTracker.Data/Models/RecurringExpenseConfig.cs
+++ src/ChefKnifeStudios.ExpenseTracker.Data/Models/RecurringExpenseConfig.cs
@@ -1,7 +1,6 @@
 ﻿namespace ChefKnifeStudios.ExpenseTracker.Data.Models;
 public class RecurringExpenseConfig : BaseEntity
 {
-    public int Id { get; set; }
     public required string Name { get; set; }
     public decimal Cost { get; set; }
     public required string LabelsJson { get; set; }
