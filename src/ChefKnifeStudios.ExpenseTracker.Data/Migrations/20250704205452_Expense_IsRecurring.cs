3ae43617a33e215814348e20cfdcd95ff7f9d68e
Henry Faulkner <hfaulkner27@gmail.com>
2025-07-04 16:56:02 -0400

    Add migration for Expense.IsRecurring

---
+++ src/ChefKnifeStudios.ExpenseTracker.Data/Migrations/20250704205452_Expense_IsRecurring.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChefKnifeStudios.ExpenseTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Expense_IsRecurring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRecurring",
+                schema: "ExpenseTracker",
+                table: "Expenses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRecurring",
+                schema: "ExpenseTracker",
+                table: "Expenses");
+        }
+    }
+}
